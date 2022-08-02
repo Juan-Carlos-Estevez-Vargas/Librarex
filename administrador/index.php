32b7bb5b35825768f0a903efd497982d8893d051
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2022-08-02 15:24:20 -0500

    Creación de la interfaz del login.

--- administrador/index.php
+++ administrador/index.php
@@ -1,12 +1,38 @@
- <!DOCTYPE html>
- <html lang="en">
- <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Administrador del sitio Web</title>
- </head>
- <body>
-    Login
- </body>
- </html>
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Administrador del sitio web</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3"></div>
+            <div class="col-md-6">
+                <br/><br/><br/><br/>
+                <div class="card">
+                    <div class="card-header">Login</div>
+                    <div class="card-body">
+                        <form>
+                            <div class = "form-group">
+                                <label for="exampleInputEmail1">Usuario:</label>
+                                <input type="text" class="form-control" name="usuario" placeholder="Ingresa tu usuario.">
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">Contraseña:</label>
+                                <input type="password" class="form-control" name="contrasenia" placeholder="Ingresa tu contraseña.">
+                            </div>
+                            <button type="submit" class="btn btn-primary">Entrar al administrador</button>
+                        </form>           
+                    </div>
+                </div>
+            </div>     
+        </div>
+    </div>
+  </body>   
+</html>
