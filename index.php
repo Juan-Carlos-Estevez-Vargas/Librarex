73a0ceef1724554485a3696b7ab51694e8b1774e
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2022-08-02 14:20:22 -0500

    Integración Bootstrap 4 y BootsWatch.

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
-<?php
-echo "hola";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sitio Web Libros</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <ul class="nav navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Nav 1 <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Nav 2</a>
+            </li>
+        </ul>
+    </nav>
+</body>
+</html>
