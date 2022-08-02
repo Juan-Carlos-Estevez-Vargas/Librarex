7bbf0399562b1c68e42dd587de19f9f8b47053d8
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2022-08-02 16:45:01 -0500

    Capturando los datos enviados desde el formulario del libro.

--- administrador/seccion/productos.php
+++ administrador/seccion/productos.php
@@ -1,4 +1,12 @@
 <?php include('../template/cabecera.php'); ?>
+<?php
+    $txtID = (isset($_POST['txtID'])) ? $_POST['txtID'] : "";
+    $txtNombre = (isset($_POST['txtNombre'])) ? $_POST['txtNombre'] : "";
+    $txtImagen = (isset($_FILES['txtImagen']['name'])) ? $_FILES['txtImagen']['name'] : "";
+    $txtAccion = (isset($_POST['accion'])) ? $_POST['accion'] : "";
+
+    
+?>
 
 <div class="col-md-5">
     <div class="card">
@@ -14,13 +22,13 @@
                     <input type="text" class="form-control" name="txtNombre" id="txtNombre" placeholder="Nombre del libro">
                 </div>
                 <div class = "form-group">
-                    <label for="txtID">Imagen</label>
-                    <input type="file" class="form-control" name="txtID" id="txtID" placeholder="ID">
+                    <label for="txtImagen">Imagen</label>
+                    <input type="file" class="form-control" name="txtImagen" id="txtImagen" placeholder="ID">
                 </div>
                 <div class="btn-group" role="group" aria-label="">
-                    <button type="button" class="btn btn-success">Agregar</button>
-                    <button type="button" class="btn btn-warning">Modificar</button>
-                    <button type="button" class="btn btn-info">Cancelar</button>
+                    <button type="button" name="accion" value="Agregar" class="btn btn-success">Agregar</button>
+                    <button type="button" name="accion" value="Modificar" class="btn btn-warning">Modificar</button>
+                    <button type="button" name="accion" value="Cancelar" class="btn btn-info">Cancelar</button>
                 </div>
             </form>
         </div>
