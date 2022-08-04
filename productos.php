f9f29e7d513b80e739604cca3a0635b4e393946c
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2022-08-04 17:24:48 -0500

    Funcionalidad para mostrar los libros en la sección libros del sitio web.

--- productos.php
+++ productos.php
@@ -1,43 +1,22 @@
-<?php include("template/cabecera.php"); ?>
+<?php 
+    include("template/cabecera.php"); 
+    include("administrador/config/bd.php");
 
+    $sentenciaSQL = $conexion->prepare("SELECT * FROM libros");
+    $sentenciaSQL->execute();
+    $listaLibros = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<?php foreach ($listaLibros as $libro) { ?>
 <div class="col-md-3">  
     <div class="card">
-        <img class="card-img-top" src="https:/www.w3schools.com/bootstrap4/img_avatar1.png" alt="">
+        <img class="card-img-top" src="./img/<?php echo $libro["imagen"]; ?>" alt="">
         <div class="card-body">
-            <h4 class="card-title">Libro PHP</h4>
+            <h4 class="card-title"><?php echo $libro["nombre"]; ?></h4>
             <a name="" id="" class="btn btn-primary" href="#" role="button"> Ver más </a>
         </div>
     </div>
 </div>
-
-<div class="col-md-3">  
-    <div class="card">
-        <img class="card-img-top" src="https:/www.w3schools.com/bootstrap4/img_avatar1.png" alt="">
-        <div class="card-body">
-            <h4 class="card-title">Title</h4>
-            <a name="" id="" class="btn btn-primary" href="#" role="button"> Ver más </a>
-        </div>
-    </div>
-</div>
-
-<div class="col-md-3">  
-    <div class="card">
-        <img class="card-img-top" src="https:/www.w3schools.com/bootstrap4/img_avatar1.png" alt="">
-        <div class="card-body">
-            <h4 class="card-title">Title</h4>
-            <a name="" id="" class="btn btn-primary" href="#" role="button"> Ver más </a>
-        </div>
-    </div>
-</div>
-
-<div class="col-md-3">  
-    <div class="card">
-        <img class="card-img-top" src="https:/www.w3schools.com/bootstrap4/img_avatar1.png" alt="">
-        <div class="card-body">
-            <h4 class="card-title">Title</h4>
-            <a name="" id="" class="btn btn-primary" href="#" role="button"> Ver más </a>
-        </div>
-    </div>
-</div>
+<?php } ?>
 
 <?php include("template/pie.php"); ?>
