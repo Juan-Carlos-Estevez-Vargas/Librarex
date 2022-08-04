aea6c9f5b202b1645224391adcd6f23149d82794
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2022-08-04 14:34:12 -0500

    Funcionalidad para cargar las imágenes a la carpeta img del proyecto.

--- administrador/seccion/productos.php
+++ administrador/seccion/productos.php
@@ -12,7 +12,17 @@
         case "Agregar":
             $sentenciaSQL = $conexion->prepare("INSERT INTO libros (nombre, imagen) VALUES (:nombre, :imagen);"); 
             $sentenciaSQL->bindParam(':nombre', $txtNombre);
-            $sentenciaSQL->bindParam(':imagen', $txtImagen);
+
+            $fecha = new DateTime();
+            $nombreArchivo = ($txtImagen != "") ? $fecha->getTimestamp()."_".$_FILES["txtImagen"]["name"] : "default.jpg";
+
+            $tmpImagen = $_FILES["txtImagen"]["tmp_name"];
+
+            if ($tmpImagen != "") {
+                move_uploaded_file($tmpImagen, "../../img/".$nombreArchivo);
+            }
+
+            $sentenciaSQL->bindParam(':imagen', $nombreArchivo);
             $sentenciaSQL->execute();
             break;
 
